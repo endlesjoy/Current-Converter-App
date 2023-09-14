611a5c88fca78ea843e10a610ab1e5f7721bc834
endlesjoy <rasul.allanazarov@udevs.io>
2023-09-14 10:24:09 +0500

    Initial commit with default activity page

---
+++ app/src/main/java/ru/endlesjoy/currencyconverterapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package ru.endlesjoy.currencyconverterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.endlesjoy.currencyconverterapp.ui.theme.CurrencyConverterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CurrencyConverterAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android hello")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CurrencyConverterAppTheme {
+        Greeting("Android")
+    }
+}
